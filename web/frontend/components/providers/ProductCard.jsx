bc6661a335b8f4ba8a087e8c4956613d1e63bade
shinyteresa <31470582+shinyteresa@users.noreply.github.com>
2023-04-17 23:56:59 +0530

    ProductCard

---
+++ web/frontend/components/providers/ProductCard.jsx
@@ -0,0 +1,32 @@
+import { Card, FormLayout } from "@shopify/polaris"
+
+const ProductsCard = () => {
+return 
+(<Card 
+sectioned 
+primaryFooterAction={{
+    content: "Update Product", 
+    onAction: () => console.log("update product"),
+    }} 
+    secondaryFooterActions={[
+        {
+            content: 'View in Admin', 
+            onAction: () => console.log('view in admin'),
+            },
+            ]}
+            >
+                <Stack spacing="extraLoose">
+                <Stack.Item>
+                    <img src="../assets/home-trophy.png" alt="width=250" />
+                </Stack.Item>
+                <Stack.Item fill>
+                    <FormLayout>
+                        <TextField label="Product Title"></TextField>
+                        <TextField multiline={4} label="Product Description"></TextField>
+                    </FormLayout>
+                </Stack.Item>
+                </Stack>
+
+</Card>
+);
+};
